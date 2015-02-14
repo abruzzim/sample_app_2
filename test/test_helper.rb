b5dc7485655fb6da9176ee9ddd21efd65aeb0877
abruzzim <abruzzim@framework-solutions.com>
2015-02-13 19:09:07 -0500

    Add Mini-Test Reporter Gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ puts "%TEST_HELPER-I-DEBUG, running test/test_helper.rb"
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/reporters'
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
