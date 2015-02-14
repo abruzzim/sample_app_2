56d23c0914490951cd5c575b2c6c34cb1f697f75
abruzzim <abruzzim@framework-solutions.com>
2015-02-13 19:04:41 -0500

    Add Contact Page with Test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,39 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
+
+  def setup
+  	puts "%STATIC_PAGES_CONTROLLER_TEST-I-SETUP, running setup method..."
+	@base_title = "Ruby on Rails 4.2.0 Tutorial Sample App"
+  end
+
+  def teardown
+  	puts "%STATIC_PAGES_CONTROLLER_TEST-I-TEARDOWN, running teardown method..."
+  	@base_title = nil
+  end
+
+  test "should get Home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails 4.2.0 Tutorial Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
-  test "should get help" do
+  test "should get Help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails 4.2.0 Tutorial Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
-  test "should get about" do
+  test "should get About" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails 4.2.0 Tutorial Sample App"
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get Contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
