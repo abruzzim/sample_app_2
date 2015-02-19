591e1470ac2392cf763129500ac70f6b9418918d
abruzzim <abruzzim@framework-solutions.com>
2015-02-19 14:09:25 -0500

    Complete New User Form Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
-  def new
-  end
 
-  def show
-  	 @user = User.find(params[:id])
-  	 # Uncomment the following line to enable the rails server console debugger breakpoint.
-  	 #debugger
-  end
+	def new
+		@user = User.new
+	end
+
+	def show
+		 @user = User.find(params[:id])
+		 # Uncomment the following line to enable the rails server console debugger breakpoint.
+		 #debugger
+	end
+  
 end
